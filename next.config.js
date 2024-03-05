72904f68bb5e7055058e242d9af479dbaedc7c10
superbigpro <rexrex1105@gmail.com>
2024-03-06 00:09:27 +0900

    d

--- next.config.js
+++ next.config.js
@@ -1,20 +1,29 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
+  compiler: {
     styledComponents: true,
   },
 
-  webpack(conf) {
-    conf.module.rules.push({
+  webpack: (config) => {
+    config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
 
-    return conf;
+    return config;
   },
+
   typescript: {
     ignoreBuildErrors: true,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://127.0.0.1:3000/:path*",
+      },
+    ];
   },
 };
 
