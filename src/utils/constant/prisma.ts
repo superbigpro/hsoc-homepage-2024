dbb5ae8b7037b2331d5b6d36e942b5fd814dd077
leeyeonwoo <74847519+superbigpro@users.noreply.github.com>
2024-03-07 22:36:19 +0900

    add prisma client

--- src/utils/constant/prisma.ts
+++ src/utils/constant/prisma.ts
@@ -1,23 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
-// let prisma: PrismaClient | null = null;
-
-// if (typeof window === 'undefined') {
-//   if (process.env.NODE_ENV === 'production') {
-//     prisma = new PrismaClient();
-//   } else {
-//     if (!global.prisma) {
-//       global.prisma = new PrismaClient();
-//     }
-
-//     prisma = global.prisma;
-//   }
-// }
-
-// export default prisma;
-
 const prisma = new PrismaClient();
 
 export default prisma;
 
-export const student = prisma?.student;
+export const user = prisma.user; // 옵셔널 체이닝 제거
+export const application = prisma.application; // 'application'을 실제 모델 이름인 'apply'로 변경
